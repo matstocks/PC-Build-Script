6fb13b11d7a83efe7446569e7e1dc12a46445f10
intech-group <mat.stocks@intechgroup.co.uk>
2020-05-14 17:25:39 +0100

    Include edge and wait timer for deploy script

--- RunMe.bat
+++ RunMe.bat
@@ -1,6 +1,5 @@
 mkdir c:\build
 Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList 'set-executionpolicy remotesigned' -Verb RunAs}"
 powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"%~dp0download.ps1\"' -verb RunAs}"
-powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\build\PCSetup.ps1\"' -verb RunAs}"
-
-
+timeout 10
+powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\build\PC-Build-Script-master\PCSetup.ps1\"' -verb RunAs}"
