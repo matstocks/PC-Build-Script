27b03b3e1143c681448b310f9ba32c98865caf0b
intech-group <mat.stocks@intechgroup.co.uk>
2020-06-01 12:58:11 +0100

    Update download.ps1

--- download.ps1
+++ download.ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy remotesigned
-function downloadbuilder {}
+function downloadbuilder {
 Invoke-WebRequest -Uri "https://github.com/matstocks/PC-Build-Script/archive/master.zip" -OutFile "C:\build\PCBuild.zip"
 Expand-Archive C:\build\PCBuild.zip -DestinationPath C:\build\
 }
+downloadbuilder
