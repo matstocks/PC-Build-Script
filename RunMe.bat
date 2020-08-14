d42799b6a45343967065762bc6135ec62d5e2fff
Mat Stocks <mat.stocks@intechgroup.co.uk>
2020-08-14 17:53:36 +0100

    Add additional detail to the RunMe.bat file

--- RunMe.bat
+++ RunMe.bat
@@ -1,5 +1,10 @@
+REM Create a directory on the C drive to store the scripts from the repository
 mkdir c:\build
+REM Enable execution policy to allow running of scripts
 Powershell.exe -Command "& {Start-Process Powershell.exe -ArgumentList 'set-executionpolicy remotesigned' -Verb RunAs}"
+REM Download the most up to date version of the script from the repository.
 powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"%~dp0download.ps1\"' -verb RunAs}"
+REM The download.ps1 auto extracts the contents to c:\build. Wait 10 seconds for this to complete before executing the PCSetup.ps1 script
 timeout 10
+REM Execute the final build script
 powershell.exe -ExecutionPolicy Bypass -noprofile -command "&{start-process powershell -ArgumentList '-noprofile -file \"c:\build\PC-Build-Script-master\PCSetup.ps1\"' -verb RunAs}"
